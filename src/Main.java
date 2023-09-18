ead6d4586e000ce55ce25d4bb9a1302298eabdea
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-09-18 23:13:33 +0300

    1-2-3 soru çözüldü

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(shouldWakeUp(true,1));
+        System.out.println(hasTeen(26,25,32));
+        System.out.println( isCatPlaying(true,56));
     }
 
     public static boolean shouldWakeUp(boolean x, int time) {
@@ -13,6 +15,19 @@ public class Main
  if(!x) return false;
  return time <8 || time >= 20;
     }
+public static boolean hasTeen( int firstAge, int secondAge,int thirdAge){
+        return (firstAge >=13 && firstAge<=19)||
+                (secondAge >=13 && secondAge<=19)||
+                (thirdAge >=13 && thirdAge<=19);
+
+
+}
+public static boolean isCatPlaying(boolean yaz, int temp){
+    if(!yaz) return (temp>25 && temp<=35);
+    else return (temp>25 && temp <= 45);
+
+
+}
 
 
 
