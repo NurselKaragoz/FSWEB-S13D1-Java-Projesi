59373a75b02f30b98e71e5c2b9c75ca1d8c97fc2
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-09-18 22:00:15 +0300

    1. soru çözüldü

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+
+
+public class Main
+
+{
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(shouldWakeUp(true,1));
     }
+
+    public static boolean shouldWakeUp(boolean x, int time) {
+ if (time <0 ||  time >23) return false;
+ if(!x) return false;
+ return time <8 || time >= 20;
+    }
+
+
+
 }
+
